2b5f3eea0e7ab33e8b18714653f798c98b60ab56
juphoff <devnull@localhost>
1996-05-22 18:58:22 -0400

    Added IS_TIMEOUT for future timeout scheme.

--- sniff.h
+++ sniff.h
@@ -3,6 +3,7 @@
 /* General definitions. */
 #define IS_BUFSIZ 2048		/* Bigger than necessary. */
 #define IS_MAXDATA 8192		/* Overrideable on the command line. */
+#define IS_TIMEOUT 60		/* Seconds idle to timeout a node. */
 
 /* Function prototypes. */
 extern void open_interface (void);
