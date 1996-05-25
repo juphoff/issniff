d99b6f0aa8929e98e5fab6f1455fb44843be816e
juphoff <devnull@localhost>
1996-05-24 20:17:07 -0400

    Rearranged.  Diddled comments.

--- sniff.h
+++ sniff.h
@@ -1,17 +1,31 @@
 /* $Id$ */
 
-/* General definitions. */
-#define CACHE_INC 16		/* Cache block increment size. */
-#define CACHE_SLACK (cache_increment >> 1) /* Management slack. */
+/*
+ * General definitions
+ */
+/*  "Hard" definitions. */
 #define IS_BUFSIZ 2048		/* Bigger than necessary. */
-#define IS_MAXDATA 8192		/* Overrideable on the command line. */
-#define IS_TIMEOUT 3600		/* Seconds idle to timeout a node. */
 
-/* Function prototypes. */
+/*  "Soft" (overrideable on the command line) definitions. */
+#define CACHE_INC 16		/* Cache block increment. */
+#define IS_MAXDATA 4096		/* Max data for connection: one page for fun. */
+#define IS_TIMEOUT 3600		/* Max idle time for connection: one hour. */
+
+/*
+ * Function prototypes.
+ */
 extern char *get_interface (void);
 extern void close_interface (int);
 extern void open_interface (void);
 extern void set_interface (const char *);
 
-/* Global variables. */
+/*
+ * Global variables.
+ */
 int iface;
+
+/*
+ * Misc. macros.
+ */
+#define CACHE_SLACK (cache_increment >> 1) /* Management slack. */
+#define YN(X) ((X) ? "yes" : "no")
