b555a0c9bf9f8e40bf579db3cb9ba695fc9dc0dd
juphoff <devnull@localhost>
1996-05-25 18:55:23 -0400

    Diddled comments.

--- linux.h
+++ linux.h
@@ -1,18 +1,22 @@
 /* $Id$ */
 
-#include <linux/if_ether.h>	/* Not yet used. */
+#include <linux/if_ether.h>
 #include <net/if.h>
 #include <netinet/ip.h>
 #include <netinet/protocols.h>
 #include <netinet/tcp.h>
 
-/* Some general definitions. */
+/*
+ * General definitions.
+ */
 #define DEFAULT_INTERFACE "eth0"
 #define SOCKPROT 0x300
 #define UCHAR u_char
 #define UINT u_int
 
-/* IP protocol header definitions. */
+/*
+ * IP protocol header definitions.
+ */
 #define IPPROT(X) ((X)->protocol)
 #define DADDR(X) ((X)->daddr)
 #define SADDR(X) ((X)->saddr)
@@ -21,7 +25,9 @@
 #define TCPPROT IP_TCP
 #define ADDR_T __u32
 
-/* TCP protocol header definitions. */
+/*
+ * TCP protocol header definitions
+ */
 #define SPORT(X) ((X)->th_sport)
 #define DPORT(X) ((X)->th_dport)
 #define DOFF(X) ((X)->th_off * 4)
@@ -29,6 +35,9 @@
 #define FINRST(X) ((X)->th_flags & (TH_FIN | TH_RST))
 #define PORT_T u_short
 
+/*
+ * Data types.
+ */
 typedef struct ethhdr ETHhdr;
 typedef struct iphdr IPhdr;
 typedef struct tcphdr TCPhdr;
