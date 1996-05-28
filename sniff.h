1f51372a1706adb536563e95cdf7c9f563f8704c
juphoff <devnull@localhost>
1996-05-28 13:13:26 -0400

    Moved IF_BUFSIZ #define to OS-specific header.  Added some function prototypes, moved iface to OS-specific file; it's no longer global due to the location of the primary read() loop.  Diddled macros slightly.

--- sniff.h
+++ sniff.h
@@ -3,10 +3,6 @@
 /*
  * General definitions
  */
-/*  "Hard" definitions. */
-#define IS_BUFSIZ 2048		/* Bigger than necessary. */
-
-/*  "Soft" (overrideable on the command line) definitions. */
 #define CACHE_INC 16		/* Cache block increment. */
 #define IS_MAXDATA 4096		/* Max data for connection: one page for fun. */
 #define IS_TIMEOUT 3600		/* Max idle time for connection: one hour. */
@@ -16,16 +12,13 @@
  */
 extern char *get_interface (void);
 extern void close_interface (int);
+extern void filter (UCHAR *);
+extern void ifread (void);
 extern void open_interface (void);
 extern void set_interface (const char *);
 
 /*
- * Global variables.
- */
-int iface;
-
-/*
  * Misc. macros.
  */
-#define CACHE_SLACK (cache_increment >> 1) /* Management slack. */
+#define CHKOPT(FALLBACK) (atoi (optarg) ? atoi (optarg) : (FALLBACK))
 #define YN(X) ((X) ? "yes" : "no")
