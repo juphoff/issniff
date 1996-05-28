a6a7107a3f8462579dd295a9d224de4fe5170a47
juphoff <devnull@localhost>
1996-05-28 13:10:21 -0400

    Moved primary read() loop here; this can be OS-dependent.  Diddled #include's.

--- linux.c
+++ linux.c
@@ -3,11 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <net/if.h>
 #include <sys/ioctl.h>
 #include <sys/socket.h>
-#include "sniff.h"
+#include <linux/if_ether.h>
 #include "linux.h"
+#include "sniff.h"
 
+static int iface;
 static struct ifreq ifr;
 
 /*
@@ -45,9 +49,9 @@ open_interface (void)
     perror ("socket");
     exit (errno);
   }
-  if (!*ifr.ifr_name)
+  if (!*ifr.ifr_name) {
     set_interface (DEFAULT_INTERFACE);
-
+  }
   if (ioctl (iface, SIOCGIFFLAGS, &ifr) == -1) {
     perror ("ioctl (SIOCGIFFLAGS)");
     exit (errno);
@@ -59,4 +63,19 @@ open_interface (void)
     exit (errno);
   }
   fprintf (stderr, "Listening on %s.\n\n", ifr.ifr_name);
+}
+
+/*
+ * I DON'T like this approach.  Blasted SunOS madness....
+ */
+void
+ifread (void)
+{
+  UCHAR buf[IF_BUFSIZ];
+
+  for (;;) {
+    if (read (iface, buf, IF_BUFSIZ) >= 0) {
+      filter (buf + sizeof (struct ethhdr));
+    }
+  }
 }
