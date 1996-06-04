77678788ab5215fde15665fb2b8961e8764b4002
juphoff <devnull@localhost>
1996-06-03 20:14:46 -0400

    Changed many #define's to typedef's.

--- linux.h
+++ linux.h
@@ -9,10 +9,6 @@
  */
 #define IF_BUFSIZ 2048
 #define IF_DETECT_BUFSIZ 1024	/* Big enough for 25 interfaces + slop. */
-#define SOCKPROT 0x300		/* Don't fuck with me. */
-#define UCHAR u_char
-#define UINT u_int
-#define UDATA unsigned short int
 
 /*
  * IP protocol header definitions.
@@ -23,7 +19,6 @@
 #define IPHLEN(X) ((X)->ihl * 4)
 #define IPLEN(X) ((X)->tot_len)
 #define TCPPROT IP_TCP
-#define ADDR_T __u32
 
 /*
  * TCP protocol header definitions
@@ -33,10 +28,14 @@
 #define DOFF(X) ((X)->th_off * 4)
 #define SYN(X) ((X)->th_flags & TH_SYN)
 #define FINRST(X) ((X)->th_flags & (TH_FIN | TH_RST))
-#define PORT_T u_short
 
 /*
  * Data types.
  */
+typedef __u32 ADDR_T;
+typedef u_short PORT_T;
+typedef u_char UCHAR;
+typedef unsigned short int UDATA;
+typedef u_int UINT;
 typedef struct iphdr IPhdr;
 typedef struct tcphdr TCPhdr;
