55cd7e9a0847dede37bab76c1597e064f27e0e3e
juphoff <devnull@localhost>
1996-06-05 02:51:46 -0400

    Added BPF support to speed filtering.  "Reworded" if_read(), rearranged code, diddled comments.

--- sunos.c
+++ sunos.c
@@ -9,10 +9,12 @@
 #include <sys/ioctl.h>
 #include <sys/sockio.h>
 #include <sys/time.h>
+#include <net/nit_if.h>
+#include <net/nit_pf.h>
+#include <net/packetfilt.h>
 #include "sunos.h"
 #include "sniff.h"
 #include <netinet/if_ether.h>
-#include <net/nit_if.h>
 
 /*
  * Local variables.
@@ -20,13 +22,14 @@
 static int iface;
 static int linkhdr_len;
 static struct ifreq ifr;
+
 /* SunOS interface type prefixes and their link-level packet header sizes. */
 static struct {
   const char *type;
   int hdr_len;
 } if_types[] = {
   { "le", sizeof (struct ether_header) },
-  { NULL }			/* Any others for SunOS?  Not soon! */
+  { NULL }			/* Any others for SunOS?  Not anytime soon! */
 };
 
 /*
@@ -35,12 +38,10 @@ static struct {
 static char *if_detect (void);
 
 /*
- * Stuff not prototyped in SunOS (that I can find).
+ * Stuff not prototyped in SunOS (that I can find).  Argh.
  */
 extern int ioctl (int, int, caddr_t);
-extern int socket(int, int, int);
-extern void bcopy (char *, char *, int);
-extern void bzero (char *, int);
+extern int socket (int, int, int);
 
 /*
  * Signal handler.
@@ -76,22 +77,62 @@ if_setname (const char *interface)
   return -1;
 }
 
+/* Consolidate. */
+static char *
+if_detect (void)
+{
+  char ifb[IF_DETECT_BUFSIZ];
+  int dummysock;		/* Just want a quickie there lass. */
+  struct ifconf ifs;
+
+  if ((dummysock = socket (AF_INET, SOCK_RAW, 0)) == -1) {
+    perror ("socket");
+    return NULL;
+  }
+  ifs.ifc_len = sizeof (ifb);
+  memset (ifs.ifc_buf = (caddr_t)&ifb, 0, sizeof (ifb));
+
+  if (ioctl (dummysock, SIOCGIFCONF, (char *)&ifs) < 0) {
+    perror ("ioctl (SIOCGIFCONF)");
+    close (dummysock);
+    return NULL;
+  } else {
+    int i = -1;
+
+    while (if_types[++i].type) {
+      struct ifreq *ifrp = ifs.ifc_req;
+
+      while (*ifrp->ifr_name) {
+	if (!strncmp (if_types[i].type, ifrp->ifr_name,
+		      strlen (if_types[i].type))) {
+	  close (dummysock);
+	  return ifrp->ifr_name;
+	}
+	++ifrp;
+      }
+    }
+    close (dummysock);
+    return NULL;
+  }
+}
+
+/*
+ * nit-picky!
+ */
 void
 if_open (int nolocal)
 {
+  struct packetfilt pf;
   struct strioctl si;
   struct timeval to;
   u_int if_bufsiz = IF_BUFSIZ;
   u_long if_flags = NI_PROMISC;
-#if 0
-  u_long snaplen = 0;
-#endif
 
   if (!*ifr.ifr_name) {
     char *interface = if_detect ();
 
     if (!interface) {
-      fprintf (stderr, "Cannot auto-detect a default interface.  Odd, that\n");
+      fprintf (stderr, "Cannot auto-detect a default interface.  Odd, that.\n");
       exit (1);
     }
     assert (if_setname (interface) == 0);
@@ -100,19 +141,25 @@ if_open (int nolocal)
     perror ("open");
     exit (errno);
   }
+  /* Read & discard. */
   if (ioctl (iface, I_SRDOPT, (char *)RMSGD) < 0) {
     perror ("ioctl (I_SRDOPT)");
     exit (errno);
   }
-  si.ic_timout = INFTIM;
-
+  /* Streams stack.  "pf" must precede "nbuf" for filtering to work right. */
+  if (ioctl (iface, I_PUSH, "pf") < 0) {
+    perror ("ioctl (I_PUSH)");
+    exit (errno);
+  }
   if (ioctl (iface, I_PUSH, "nbuf") < 0) {
     perror ("ioctl (I_PUSH)");
     exit (errno);
   }
-  to.tv_sec = 1;
-  to.tv_usec = 0;
+  si.ic_timout = INFTIM;
+  /* Set buffer flush timer. */
   si.ic_cmd = NIOCSTIME;
+  to.tv_sec = IF_BUF_TIMER;
+  to.tv_usec = 0;
   si.ic_len = sizeof (to);
   si.ic_dp = (char *)&to;
 
@@ -120,12 +167,31 @@ if_open (int nolocal)
     perror ("ioctl (I_STR: NIOCSTIME)");
     exit (errno);
   }
+  /* Set nit buffer size.  Big means fewer read()'s, but more delay. */
   si.ic_cmd = NIOCSCHUNK;
   si.ic_len = sizeof (if_bufsiz);
   si.ic_dp = (char *)&if_bufsiz;
 
   if (ioctl (iface, I_STR, (char *)&si) < 0) {
     perror ("ioctl (I_STR: NIOCSCHUNK)");
+    exit (errno);
+  }
+  /* Keep the high-level filtering in the kernel...we're not interested! */
+  si.ic_cmd = NIOCSETF;
+  pf.Pf_FilterLen = 0;
+  pf.Pf_Filter[pf.Pf_FilterLen++] = ENF_PUSHWORD + 11;
+  pf.Pf_Filter[pf.Pf_FilterLen++] = ENF_PUSHLIT | ENF_AND;
+  pf.Pf_Filter[pf.Pf_FilterLen++] = 0x00FF; /* Proto. is u_char on lower end. */
+  pf.Pf_Filter[pf.Pf_FilterLen++] = ENF_PUSHLIT | ENF_CAND;
+  pf.Pf_Filter[pf.Pf_FilterLen++] = htons ((u_short)IPPROTO_TCP);
+  pf.Pf_Filter[pf.Pf_FilterLen++] = ENF_PUSHWORD + 6;
+  pf.Pf_Filter[pf.Pf_FilterLen++] = ENF_PUSHLIT | ENF_EQ;
+  pf.Pf_Filter[pf.Pf_FilterLen++] = htons (ETHERTYPE_IP);
+  si.ic_len = sizeof (pf);
+  si.ic_dp = (char *)&pf;
+  
+  if (ioctl (iface, I_STR, (char *)&si) < 0) {
+    perror ("ioctl (I_STR: NIOCSETF)");
     exit (errno);
   }
   /* No check here to see if it's up; I'll just let the ioctl() fail for now. */
@@ -137,6 +203,7 @@ if_open (int nolocal)
     perror ("ioctl (I_STR: NIOCBIND)");
     exit (errno);
   }
+  /* Get promiscous. */
   si.ic_cmd = NIOCSFLAGS;
   si.ic_len = sizeof (if_flags);
   si.ic_dp = (char *)&if_flags;
@@ -145,86 +212,40 @@ if_open (int nolocal)
     perror ("ioctl (I_STR: NIOCSFLAGS)");
     exit (errno);
   }
-#if 0
-  si.ic_cmd = NIOCSSNAP;
-  si.ic_len = sizeof (snaplen);
-  si.ic_dp = (char *)&snaplen;
-  if (ioctl (iface, I_STR, (char *)&si) < 0) {
-    perror ("ioctl (I_STR: NIOCSSNAP)");
-    exit (errno);
-  }
-#endif
+  /* Flush the can before sitting down. */
   if (ioctl (iface, I_FLUSH, (char *)FLUSHR) < 0) {
     perror ("ioctl (I_FLUSH)");
     exit (errno);
   }
   fprintf (stderr, "Listening on %s.\n", ifr.ifr_name);
+  /* I want an option for local stuff, but I don't know how to do it yet! */
   fprintf (stderr, "Warning: locally-originating packets not monitored!\n\n");
 }
 
-/* Consolidate. */
-static char *
-if_detect (void)
-{
-  char ifb[IF_DETECT_BUFSIZ];
-  int dumsock;
-  struct ifconf ifs;
-
-  if ((dumsock = socket (AF_INET, SOCK_RAW, 0)) == -1) {
-    perror ("socket");
-    exit (errno);
-  }
-  ifs.ifc_len = sizeof (ifb);
-  memset (ifs.ifc_buf = (caddr_t)&ifb, 0, sizeof (ifb));
-
-  if (ioctl (dumsock, SIOCGIFCONF, (char *)&ifs) < 0) {
-    perror ("ioctl (SIOCGIFCONF)");
-    close (dumsock);
-    return NULL;
-  } else {
-    int i = -1;
-
-    while (if_types[++i].type) {
-      struct ifreq *ifrp = ifs.ifc_req;
-
-      while (*ifrp->ifr_name) {
-	if (!strncmp (if_types[i].type, ifrp->ifr_name,
-		      strlen (if_types[i].type))) {
-	  close (dumsock);
-	  return ifrp->ifr_name;
-	}
-	++ifrp;
-      }
-    }
-    close (dumsock);
-    return NULL;
-  }
-}
-
 /*
- * This needs work!
+ * This could probably be tightened up a little.
  */
 void
 if_read (void)
 {
-  int cc;
-  struct nit_bufhdr *hdrp;
-  UCHAR abuf[IF_BUFSIZ], rbuf[IF_BUFSIZ];
-  UCHAR *bp, *bufstop, *cp;
+  int bytes;
+  struct nit_bufhdr *bufhdrp;
+  UCHAR aligned_buf[IF_BUFSIZ], rawbuf[IF_BUFSIZ];
+  UCHAR *bufp, *bufstop, *pktp;
 
-  /* Need to use nit_pf to leave all but TCP/IP out of the read() stream. */
-  while ((cc = read (iface, rbuf, IF_BUFSIZ)) >= 0) {
-    bp = rbuf;
-    bufstop = rbuf + cc;
+  while ((bytes = read (iface, rawbuf, IF_BUFSIZ)) >= 0) {
+    bufp = rawbuf;
+    bufstop = rawbuf + bytes;
 
-    while (bp < bufstop) {
-      cp = bp;
-      hdrp = (struct nit_bufhdr *)cp;
-      cp += sizeof (struct nit_bufhdr);
-      bp += hdrp->nhb_totlen;
-      bcopy ((char *)cp + linkhdr_len, (char *)abuf,
-	     (int)(hdrp->nhb_msglen - linkhdr_len));
-      filter (abuf);
+    /* Buffering makes this fun! */
+    while (bufp < bufstop) {
+      pktp = bufp;
+      bufhdrp = (struct nit_bufhdr *)pktp;
+      pktp += sizeof (*bufhdrp);
+      bufp += bufhdrp->nhb_totlen;
+      memcpy ((char *)aligned_buf, (char *)pktp + linkhdr_len, /* Align! */
+	      (int)(bufhdrp->nhb_msglen - linkhdr_len));
+      filter (aligned_buf);
     }
   }
 }
