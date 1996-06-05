6a1a2a7aba7e8282df2e0e42ac56c1d7628e8719
juphoff <devnull@localhost>
1996-06-05 02:47:11 -0400

    Diddled comments.

--- sniff.h
+++ sniff.h
@@ -3,8 +3,8 @@
 /*
  * General definitions
  */
-#define CACHE_INC 16		/* Cache block increment. */
-#define IS_MAXDATA 4096		/* Max data for connection: one page for fun. */
+#define CACHE_INC 16		/* Cache block increment, arbitrary. */
+#define IS_MAXDATA 4096		/* Max data for connection, arbitrary. */
 #define IS_TIMEOUT 3600		/* Max idle time for connection: one hour. */
 
 /*
