7693df1de9a53b63da0973e72ef0e9bf648de660
juphoff <devnull@localhost>
1996-06-06 23:20:13 -0400

    OSF/1 additions.

--- if.h
+++ if.h
@@ -8,13 +8,27 @@ extern char *if_detect (int);
 #define USE_TEMP_SOCK -1
 
 #ifdef __linux__
+# include <net/if.h>
 # include <linux/if_ether.h>
 #endif
 
 #ifdef __sun__
+# include <net/if.h>
 # include <netinet/in_systm.h>
 # include <netinet/in.h>
 # include <netinet/if_ether.h>
 
 extern int ioctl (int, int, caddr_t);
+#endif
+
+#ifdef __osf__
+# include <sys/mbuf.h>
+# include <sys/socket.h>
+# include <net/route.h>
+# include <net/if.h>
+# include <net/if_arp.h>
+# include <netinet/in.h>
+# include <netinet/if_ether.h>
+
+extern int ioctl (int, int, ...);
 #endif
