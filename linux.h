b99b36437710d8af9e3bbc874d87e89994597f29
juphoff <devnull@localhost>
1996-06-26 10:24:17 -0400

    Added some Ethernet header definitions.

--- linux.h
+++ linux.h
@@ -22,13 +22,19 @@
 #define TCPPROT IP_TCP
 
 /*
- * TCP protocol header definitions
+ * TCP protocol header definitions.
  */
 #define SPORT(X) ((X)->th_sport)
 #define DPORT(X) ((X)->th_dport)
 #define DOFF(X) ((X)->th_off * 4)
 #define SYN(X) ((X)->th_flags & TH_SYN)
 #define FINRST(X) ((X)->th_flags & (TH_FIN | TH_RST))
+
+/*
+ * Ethernet header definitions.
+ */
+#define ETHTYPE(X) ((X)->h_proto)
+#define IPTYPE (ntohs (ETH_P_IP))
 
 /*
  * Data types.
@@ -40,3 +46,4 @@ typedef unsigned short int UDATA;
 typedef u_int UINT;
 typedef struct iphdr IPhdr;
 typedef struct tcphdr TCPhdr;
+typedef struct ethhdr ETHhdr;
