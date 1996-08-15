f03f317ea30994bfa5ec352036a2d664dbdb2227
juphoff <devnull@localhost>
1996-08-15 18:57:13 -0400

    Added prototypes for 'tcpdump' "interface" functions, renamed 'if_read_ip' prototype to 'if_read_ip_net'.

--- sniff.h
+++ sniff.h
@@ -21,7 +21,14 @@ extern char *if_getname (void);
 extern int if_setname (const char *);
 extern void if_close_net (int);
 extern void if_open_net (int);
-extern void if_read_ip (void (*) (UCHAR *, int));
+extern void if_read_ip_net (void (*) (UCHAR *, int));
+
+#ifdef SUPPORT_TCPDUMP
+extern void if_close_pcap (int);
+extern void if_open_pcap (int);
+extern void if_read_ip_pcap (void (*) (UCHAR *, int));
+#endif
+
 #if 0
 extern void if_read_ip_raw (void (*) (UCHAR *, int));
 #endif
