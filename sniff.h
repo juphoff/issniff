b65b35f872ca42191e91801606f09df634feeded
juphoff <devnull@localhost>
1997-03-20 00:05:34 -0500

    Moved some general and color definitions to misc.h.

--- sniff.h
+++ sniff.h
@@ -1,20 +1,6 @@
 /* $Id$ */
 
 /*
- * General definitions
- */
-#define CACHE_INC 16		/* Cache block increment, arbitrary. */
-#define IS_MAXDATA 4096		/* Max data for connection, arbitrary. */
-#define IS_TIMEOUT 3600		/* Max idle time for connection: one hour. */
-
-/*
- * Colors.  Feel free to diddle....
- */
-#define FROM_COLOR 36		/* Cyan. */
-#define TO_COLOR 33		/* Yellow. */
-#define NO_COLOR 0
-
-/*
  * Function prototypes.
  */
 extern char *if_getname (void);
