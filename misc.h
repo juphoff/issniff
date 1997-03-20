0950b8362f4a3414fc467867bf8f8b4dd2affeb5
juphoff <devnull@localhost>
1997-03-20 00:06:04 -0500

    Moved some general and color definitions here from sniff.h.

---
+++ misc.h
@@ -0,0 +1,15 @@
+/* $Id$ */
+
+/*
+ * Miscellanous general definitions.
+ */
+#define CACHE_INC 16		/* Cache block increment, arbitrary. */
+#define IS_MAXDATA 4096		/* Max data for connection, arbitrary. */
+#define IS_TIMEOUT 3600		/* Max idle time for connection: one hour. */
+
+/*
+ * Colors.  Feel free to diddle....
+ */
+#define FROM_COLOR 34		/* Blue. */
+#define TO_COLOR 31		/* Red. */
+#define NO_COLOR 0
