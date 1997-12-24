1ffaa660445a6d6cd860e7b47d03330b36b1f841
juphoff <devnull@localhost>
1997-12-24 16:13:04 -0500

    Implemented PPP under Linux.

--- if_linux.h
+++ if_linux.h
@@ -8,9 +8,9 @@ static struct {
   int hdr_len;
 } if_types[] = {
   { "eth", sizeof (struct ethhdr) },
+  { "ppp", 0 },
   { "sl", 0 },
   { "lo", sizeof (struct ethhdr) },
   { "dummy", sizeof (struct ethhdr) },
-  /* Still need PPP. */
   { NULL }
 };
