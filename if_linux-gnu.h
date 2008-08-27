d6308eab47f654e8f819bf804d8c422b4daf0bb4
Jeff Uphoff <https://issues.rpath.com/>
2008-08-27 12:34:36 -0400

    add wlan & tun interface type defs

--- if_linux-gnu.h
+++ if_linux-gnu.h
@@ -8,6 +8,8 @@ static struct {
   int hdr_len;
 } if_types[] = {
   { "eth", sizeof (struct ethhdr) },
+  { "wlan", sizeof (struct ethhdr) },
+  { "tun", 0 },
   { "ppp", 0 },
   { "sl", 0 },
   { "lo", sizeof (struct ethhdr) },
