082c2d0d0030f7939d8df007449c6ec4b3373773
Jeff Uphoff <juphoff@eucalyptus.com>
2013-02-19 17:05:16 -0500

    add em & br type interfaces for Linux

--- if_linux-gnu.h
+++ if_linux-gnu.h
@@ -6,6 +6,8 @@ static struct {
   int hdr_len;
 } if_types[] = {
   { "eth", sizeof (struct ethhdr) },
+  { "em", sizeof (struct ethhdr) },
+  { "br", sizeof (struct ethhdr) },
   { "wlan", sizeof (struct ethhdr) },
   { "tun", 0 },
   { "ppp", 0 },
