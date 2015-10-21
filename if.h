75f62a98f3e227ea5e6a7a1719319a5870e9195b
Jeff Uphoff <jau@datera.io>
2015-10-21 14:48:30 -0400

    Removed OSF/1 functionality

--- if.h
+++ if.h
@@ -9,15 +9,3 @@ extern char *if_detect (int);
 # include <net/if.h>
 # include <linux/if_ether.h>
 #endif /* __linux__ */
-
-#ifdef __osf__
-# include <sys/mbuf.h>
-# include <sys/socket.h>
-# include <net/route.h>
-# include <net/if.h>
-# include <net/if_arp.h>
-# include <netinet/in.h>
-# include <netinet/if_ether.h>
-
-extern int ioctl (int, int, ...);
-#endif /* __osf__ */
